6f9c4447eb69ba5109747894864ab7c31e9039b8
vfaconi <vanessafaconi@gmail.com>
2021-01-16 00:19:36 -0300

    init

--- lesson2/js/main.js
+++
@@ -1,9 +0,0 @@
-let now = new Date();
-    let month = now.getMonth()+1;
-    let dayNumber = now.getDate();
-    let year = now.getFullYear();
-    let hour = now.getHours();
-    let minute = now.getMinutes();
-    let second = now.getSeconds();
-    let fullDate = month + "/" + dayNumber + "/" + year + " " + hour + ":" + minute + ":" + second;
-    document.getElementById("currentDate").textContent = fullDate;
