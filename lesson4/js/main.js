0ec244db72ad35577e98b9f12c1ba6ba8d0bd673
vfaconi <vanessafaconi@gmail.com>
2021-01-30 18:11:30 -0300

    init

---
+++ lesson4/js/main.js
@@ -0,0 +1,14 @@
+let now = new Date();
+    let month = now.getMonth()+1;
+    let dayNumber = now.getDate();
+    let year = now.getFullYear();
+    let hour = now.getHours();
+    let minute = now.getMinutes();
+    let second = now.getSeconds();
+    let fullDate = month + "/" + dayNumber + "/" + year + " " + hour + ":" + minute + ":" + second;
+    document.getElementById("currentDate").textContent = fullDate;
+
+function toggleMenu(){
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+
+}
