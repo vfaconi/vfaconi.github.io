6523d7ca59da7094d2db7e348e781e31155174e5
vfaconi <vanessafaconi@gmail.com>
2021-03-13 21:09:13 -0300

    init

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,4 +1,4 @@
-const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=c18d3183d63b91860166aa45180de0ae"
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=c18d3183d63b91860166aa45180de0ae"
 
 //weather api activity
 fetch(apiURL)
