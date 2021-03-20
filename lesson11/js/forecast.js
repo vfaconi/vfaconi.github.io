34503cc1725f9d001fc75729e4709b19fe2f0ae2
vfaconi <vanessafaconi@gmail.com>
2021-03-20 17:34:01 -0300

    init

---
+++ lesson11/js/forecast.js
@@ -0,0 +1,23 @@
+ //5 day forecast
+
+ const forecastURL = "http://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=c18d3183d63b91860166aa45180de0ae&units=imperial"
+
+ fetch(forecastURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+  //console.log(jsObject);
+  let day = 0;
+  const dayofWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  for(let i=0; i<jsObject.list.length; i++){
+   let d = new Date(jsObject.list[i].dt_txt);
+   let time = jsObject.list[i].dt_txt;
+   if(time.includes("18:00:00")){
+    document.getElementById(`dayofWeek${day+1}`).textContent = dayofWeek[d.getDay()];
+    document.getElementById(`forecast${day+1}`).textContent = jsObject.list[i].main.temp;
+    const imagesrc = 'https://openweathermap.org/img/wn/' + jsObject.list[i].weather[0].icon + '.png';
+    const desc = jsObject.list[i].weather[0].description;
+    document.getElementById(`icon${day+1}`).setAttribute('src', imagesrc);
+    day++;
+     }
+  }
+});
---
+++ lesson11/js/weatherapi.js
@@ -0,0 +1,43 @@
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=c18d3183d63b91860166aa45180de0ae"
+
+//weather api activity
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    document.getElementById('current-temp').textContent = jsObject.main.temp;
+    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
+    const desc = jsObject.weather[0].description;  // note how we reference the weather array
+    document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+    document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+    document.getElementById('icon').setAttribute('alt', desc);
+  });
+
+  //preston page
+         fetch(apiURL)
+        .then((response) => response.json())
+        .then((jsObject) => {
+
+          console.log(jsObject);
+          const descr = jsObject.weather[0].description; 
+          document.getElementById('descrip').textContent = descr;
+          document.getElementById('high').textContent = jsObject.main.temp_max;
+          const humidity = jsObject.main.humidity;
+          document.getElementById('humidity').textContent = humidity;
+          const wind = jsObject.wind.speed;
+          document.getElementById('wind').textContent = wind;
+           
+
+  //wind chill factor
+        result = 35.74 + 0.6215 * humidity - 35.75 * (Math.pow(wind, 0.16)) + 0.4275 * humidity * (Math.pow(wind, 0.16)); 
+        document.getElementById("output").textContent = Math.round(result);
+        });
+
+        
+ 
+ 
+
+     
+     
+
+ 
